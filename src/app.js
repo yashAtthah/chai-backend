a9518e1a49e4a819d1b1c771393a6c8284353049
yashdev24r <yashdev24r@gmail.com>
2025-06-12 12:57:37 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors  from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json({limit : "16kb"}));                      // To pass json data
+app.use(express.urlencoded({extended:true, limit:"16kb"}));   // To pass urlencoded data
+app.use(express.static("public"))                             // To make folder static
+app.use(cookieParser())                                       // To set-get cookies on browser
+
+export { app };
